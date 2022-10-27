2bec48e16e2a6dcea84a42dbd4d671254df2bba0
provok4tor <103079724+provok4tor@users.noreply.github.com>
2022-10-28 02:53:26 +0300

    Add files via upload
    task 2 полуцается

---
+++ FaceCheck/FaceCheck/AppConfiguration/ServicesExtensions/AddVersioningConfiguration.cs
@@ -0,0 +1,31 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaceCheck.AppConfigure.ServicesExtensions
+{
+    /// <summary>
+    /// Services extensions
+    /// </summary>
+    public static partial class ServicesExtensions
+    {
+        /// <summary>
+        /// Configure versioning
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddVersioningConfiguration(this IServiceCollection services)
+        {
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+        }
+    }
+}
