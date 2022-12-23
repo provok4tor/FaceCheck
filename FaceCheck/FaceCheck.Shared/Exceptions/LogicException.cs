13e4f43f600ae6c95dabc92f11442c37f078d0ac
provok4tor <103079724+provok4tor@users.noreply.github.com>
2022-12-23 05:35:42 +0300

    Add files via upload

---
+++ FaceCheck/FaceCheck.Shared/Exceptions/LogicException.cs
@@ -0,0 +1,15 @@
+using FaceCheck.Shared.ResultCodes;
+
+namespace FaceCheck.Shared.Exceptions;
+
+public class LogicException : Exception
+{
+    public ResultCode? Code { get; set; }
+
+    public LogicException(string message) : base(message) { }
+
+    public LogicException(ResultCode code) : base(code.GetDescription())
+    {
+        Code = code;
+    }
+}
