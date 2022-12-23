28e775a93609b7b84f17a8ead16e6b88cb49e929
provok4tor <103079724+provok4tor@users.noreply.github.com>
2022-12-23 04:51:30 +0300

    Add files via upload

--- FaceCheck/Program.cs
+++ FaceCheck/Program.cs
@@ -1,16 +1,22 @@
-using FaceCheck.AppConfigure.ApplicationExtensions;
-using FaceCheck.AppConfigure.ServicesExtensions;
+using FaceCheck.Repository;
+using FaceCheck.Services;
+using FaceCheck.WebAPI.AppConfiguration;
+using FaceCheck.WebAPI.AppConfiguration.ServicesExtensions;
+using FaceCheck.WebAPI.AppConfiguration.ApplicationExtensions;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.AddSerilogConfiguration();
+builder.Services.AddDbContextConfiguration(configuration);
+builder.Services.AddVersioningConfiguration();
+builder.Services.AddMapperConfiguration();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
-
-builder.AddSerilogConfiguration(); //Add serilog
-builder.Services.AddVersioningConfiguration(); //add api versioning
-builder.Services.AddSwaggerConfiguration(); //add swagger configuration
+builder.Services.AddSwaggerConfiguration(configuration);
+builder.Services.AddRepositoryConfiguration();
+builder.Services.AddBusinessLogicConfiguration();
+builder.Services.AddAuthorizationConfiguration(configuration); //1
 
 var app = builder.Build();
 
